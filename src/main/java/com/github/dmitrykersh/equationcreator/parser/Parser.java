7881be5c36dc3bf070a9c9e8c42372bdffbeac3d
dmitrykersh <d.karpukhin@mail.ru>
2021-02-12 01:05:02 +0300

    impl: supported any combination of inner ranges and lists. Priority: Float range -> Int range -> List

--- src/main/java/com/github/dmitrykersh/equationcreator/parser/Parser.java
+++ src/main/java/com/github/dmitrykersh/equationcreator/parser/Parser.java
@@ -96,77 +96,91 @@ public class Parser {
             equation = var_use_matcher.replaceFirst(replacement);
             var_use_matcher.reset(equation);
         }
-
-        // parse float ranges
         Matcher float_range_matcher = FLOAT_RANGE_PATTERN.matcher(equation);
-        while (float_range_matcher.find()) {
-            int start = float_range_matcher.start();
-            int end = float_range_matcher.end();
+        Matcher int_range_matcher = INT_RANGE_PATTERN.matcher(equation);
+        Matcher list_matcher = LIST_PATTERN.matcher(equation);
 
-            String range = equation.substring(start + 1, end - 1); // cutting [ and ]
-            String[] step_split = range.split(FLOAT_RANGE_STEP_DELIM);
-            String[] values = step_split[0].split(RANGE_DELIM);
+        while (float_range_matcher.find() || int_range_matcher.find() || list_matcher.find()){
+            float_range_matcher.reset(equation);
+            int_range_matcher.reset(equation);
+            list_matcher.reset(equation);
 
-            boolean stepIsDivisor = step_split[1].startsWith(":");
+            // parse float ranges
+            while (float_range_matcher.find()) {
+                int start = float_range_matcher.start();
+                int end = float_range_matcher.end();
 
-            Double randomInRange;
+                String range = equation.substring(start + 1, end - 1); // cutting [ and ]
+                String[] step_split = range.split(FLOAT_RANGE_STEP_DELIM);
+                String[] values = step_split[0].split(RANGE_DELIM);
 
-            double val0 = Double.parseDouble(values[0]);
-            double val1 = Double.parseDouble(values[1]);
+                boolean stepIsDivisor = step_split[1].startsWith(":");
+
+                Double randomInRange;
+
+                double val0 = Double.parseDouble(values[0]);
+                double val1 = Double.parseDouble(values[1]);
 
 
-            if (stepIsDivisor) {
-                // delete ':'
-                step_split[1] = step_split[1].substring(1);
-                double divisor = Double.parseDouble(step_split[1]);
-                int minQuotient = Math.floor(val0 / divisor) * divisor < val0 ? (int)Math.ceil(val0 / divisor) : (int)Math.floor(val0 / divisor);
-                int maxQuotient = Math.round(val1 / divisor) * divisor > val1 ? (int)Math.round(val1 / divisor) : (int)Math.round(val1 / divisor) + 1;
+                if (stepIsDivisor) {
+                    // delete ':'
+                    step_split[1] = step_split[1].substring(1);
+                    double divisor = Double.parseDouble(step_split[1]);
+                    int minQuotient = Math.floor(val0 / divisor) * divisor < val0 ? (int) Math.ceil(val0 / divisor)
+                            : (int) Math.floor(val0 / divisor);
+                    int maxQuotient = Math.round(val1 / divisor) * divisor > val1 ? (int) Math.round(val1 / divisor)
+                            : (int) Math.round(val1 / divisor) + 1;
 
-                int quotient = minQuotient + random.nextInt(maxQuotient - minQuotient);
+                    int quotient = minQuotient + random.nextInt(maxQuotient - minQuotient);
 
-                randomInRange = quotient * divisor;
-            } else {
-                double step = Double.parseDouble(step_split[1]);
-                int stepCount = random.nextInt(1 + (int) ((val1 - val0) / step));
-                randomInRange = val0 + stepCount * step;
+                    randomInRange = quotient * divisor;
+                } else {
+                    double step = Double.parseDouble(step_split[1]);
+                    int stepCount = random.nextInt(1 + (int) ((val1 - val0) / step));
+                    randomInRange = val0 + stepCount * step;
+                }
+
+                String decimalFormatPattern = String.valueOf(step_split[1]).replaceAll("\\d", "#");
+                DecimalFormat decimalFormat = new DecimalFormat(decimalFormatPattern);
+
+                equation = float_range_matcher.replaceFirst(decimalFormat.format(randomInRange));
+                float_range_matcher.reset(equation);
             }
 
-            String decimalFormatPattern = String.valueOf(step_split[1]).replaceAll("\\d", "#");
-            DecimalFormat decimalFormat = new DecimalFormat(decimalFormatPattern);
+            // parse integer ranges
+            while (int_range_matcher.find()) {
+                int start = int_range_matcher.start();
+                int end = int_range_matcher.end();
 
-            equation = float_range_matcher.replaceFirst(decimalFormat.format(randomInRange));
+                String range = equation.substring(start + 1, end - 1); // cutting [ and ]
+                String[] values = range.split(RANGE_DELIM);
+
+                int randomInt = Integer.parseInt(values[0]) +
+                        random.nextInt(Integer.parseInt(values[1]) - Integer.parseInt(values[0]));
+
+                equation = int_range_matcher.replaceFirst(String.valueOf(randomInt));
+                int_range_matcher.reset(equation);
+            }
+
+            // parse lists
+            while (list_matcher.find()) {
+                int start = list_matcher.start();
+                int end = list_matcher.end();
+
+                String list = equation.substring(start + 1, end - 1); // cutting { and }
+                String[] values = list.split(LIST_DELIM);
+                int randomIndex = random.nextInt(values.length);
+
+                equation = list_matcher.replaceFirst(values[randomIndex]);
+                list_matcher.reset(equation);
+            }
             float_range_matcher.reset(equation);
-        }
-
-        // parse integer ranges
-        Matcher int_range_matcher = INT_RANGE_PATTERN.matcher(equation);
-        while (int_range_matcher.find()) {
-            int start = int_range_matcher.start();
-            int end = int_range_matcher.end();
-
-            String range = equation.substring(start + 1, end - 1); // cutting [ and ]
-            String[] values = range.split(RANGE_DELIM);
-
-            int randomInt = Integer.parseInt(values[0]) +
-                    random.nextInt(Integer.parseInt(values[1]) - Integer.parseInt(values[0]));
-
-            equation = int_range_matcher.replaceFirst(String.valueOf(randomInt));
             int_range_matcher.reset(equation);
-        }
-
-        // parse lists
-        Matcher list_matcher = LIST_PATTERN.matcher(equation);
-        while (list_matcher.find()) {
-            int start = list_matcher.start();
-            int end = list_matcher.end();
-
-            String list = equation.substring(start + 1, end - 1); // cutting { and }
-            String[] values = list.split(LIST_DELIM);
-            int randomIndex = random.nextInt(values.length);
-
-            equation = list_matcher.replaceFirst(values[randomIndex]);
             list_matcher.reset(equation);
         }
+
+
+
         return equation;
     }
 }
