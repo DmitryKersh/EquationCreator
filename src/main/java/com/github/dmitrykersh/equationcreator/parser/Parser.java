598a5992307811320c52681c455505360fa4a1c8
dmitrykersh <d.karpukhin@mail.ru>
2021-02-13 14:59:19 +0300

    impl: in-line arithmetics fix: not seeing variable definition if value contained usages of another variables

--- src/main/java/com/github/dmitrykersh/equationcreator/parser/Parser.java
+++ src/main/java/com/github/dmitrykersh/equationcreator/parser/Parser.java
@@ -33,6 +33,17 @@ public class Parser {
     private static final Pattern VAR_DEFINITION_PATTERN = Pattern.compile("[a-zA-Z]+=<[^<>]+>");
     private static final Pattern VAR_USAGE_PATTERN = Pattern.compile("<[a-zA-Z]+>");
 
+    // arithmetics
+    // to use arithmetic signs for their purpose, use $
+    // 12$+23 will be replaced with 35
+    private static final Pattern ARITH_BRACKETS_PATTERN = Pattern.compile("\\(\\d+(\\.\\d+)?(\\$[+\\-/*]\\d+(\\.\\d+)?)+\\)");
+    private static final Pattern ARITH_NO_BRACKETS_PATTERN = Pattern.compile("\\d+(\\.\\d+)?(\\$[+\\-/*]\\d+(\\.\\d+)?)+");
+    private static final Pattern ARITH_PRIOR_1 = Pattern.compile("\\d+(\\.\\d+)?\\$\\^\\d+(\\.\\d+)?");
+    private static final Pattern ARITH_PRIOR_2 = Pattern.compile("\\d+(\\.\\d+)?\\$[*/]\\d+(\\.\\d+)?");
+    private static final Pattern ARITH_PRIOR_3 = Pattern.compile("\\d+(\\.\\d+)?\\$[+-]\\d+(\\.\\d+)?");
+
+    private static final String ARITH_SIGN_PREFIX = "\\$";
+
     private static final String VAR_NOT_DEFINED_WARN = "UNDEFINED VARIABLE: ";
 
     public Parser(final @NotNull String format) {
@@ -54,62 +65,67 @@ public class Parser {
 
         // parse variable definition
         Map<String, String> variables = new HashMap<>();
+        Matcher var_use_matcher = VAR_USAGE_PATTERN.matcher(equation);
         Matcher var_def_matcher = VAR_DEFINITION_PATTERN.matcher(equation);
 
-        while (var_def_matcher.find()){
-            int start = var_def_matcher.start();
-            int end = var_def_matcher.end();
+        boolean anyVariableFound = true;
 
-            String range = equation.substring(start, end);
+        while (anyVariableFound){
+            anyVariableFound = false;
 
-            // words[0] - variable name, words[1] - value
-            String[] words = range.split("=");
-
-            // cutting braces <>
-            words[1] = words[1].substring(1, words[1].length() - 1);
-
-            Parser valueParser = new Parser(words[1]);
-            String value = valueParser.createEquation(random);
-
-            variables.put(words[0], value);
-
-            equation = var_def_matcher.replaceFirst(value);
             var_def_matcher.reset(equation);
-        }
+            while (var_def_matcher.find()){
+                int start = var_def_matcher.start();
+                int end = var_def_matcher.end();
 
-        // parse variable usage
-        Matcher var_use_matcher = VAR_USAGE_PATTERN.matcher(equation);
+                String range = equation.substring(start, end);
 
-        while (var_use_matcher.find()){
-            int start = var_use_matcher.start();
-            int end = var_use_matcher.end();
+                // words[0] - variable name, words[1] - value
+                String[] words = range.split("=");
 
-            String var_name = equation.substring(start + 1, end - 1);
-            String replacement = "";
+                // cutting braces <>
+                words[1] = words[1].substring(1, words[1].length() - 1);
 
-            if (variables.containsKey(var_name)){
-                replacement = variables.get(var_name);
-            } else {
-                replacement = VAR_NOT_DEFINED_WARN.concat(var_name);
+                Parser valueParser = new Parser(words[1]);
+                String value = valueParser.createEquation(random);
+
+                variables.put(words[0], value);
+
+                equation = var_def_matcher.replaceFirst(value);
+                var_def_matcher.reset(equation);
+                anyVariableFound = true;
             }
 
-            equation = var_use_matcher.replaceFirst(replacement);
+            // parse variable usage
             var_use_matcher.reset(equation);
+            while (var_use_matcher.find()){
+                int start = var_use_matcher.start();
+                int end = var_use_matcher.end();
+
+                String var_name = equation.substring(start + 1, end - 1);
+
+                if (variables.containsKey(var_name)) {
+                    equation = var_use_matcher.replaceFirst(variables.get(var_name));
+                    anyVariableFound = true;
+                } else break;
+
+                var_use_matcher.reset(equation);
+            }
         }
+
         Matcher float_range_matcher = FLOAT_RANGE_PATTERN.matcher(equation);
         Matcher int_range_matcher = INT_RANGE_PATTERN.matcher(equation);
         Matcher list_matcher = LIST_PATTERN.matcher(equation);
+        Matcher arith_brackets_matcher = ARITH_BRACKETS_PATTERN.matcher(equation);
+        Matcher arith_no_brackets_matcher = ARITH_NO_BRACKETS_PATTERN.matcher(equation);
 
         boolean anyPatternFound = true;
 
         while (anyPatternFound){
             anyPatternFound = false;
 
-            float_range_matcher.reset(equation);
-            int_range_matcher.reset(equation);
-            list_matcher.reset(equation);
-
             // parse float ranges
+            float_range_matcher.reset(equation);
             while (float_range_matcher.find()) {
                 int start = float_range_matcher.start();
                 int end = float_range_matcher.end();
@@ -151,8 +167,10 @@ public class Parser {
                 float_range_matcher.reset(equation);
                 anyPatternFound = true;
             }
+            // if (anyPatternFound) continue;
 
             // parse integer ranges
+            int_range_matcher.reset(equation);
             while (int_range_matcher.find()) {
                 int start = int_range_matcher.start();
                 int end = int_range_matcher.end();
@@ -167,8 +185,37 @@ public class Parser {
                 int_range_matcher.reset(equation);
                 anyPatternFound = true;
             }
+            if (anyPatternFound) continue;
+
+            // compute arithmetics
+            arith_brackets_matcher.reset(equation);
+            while (arith_brackets_matcher.find()){
+                int start = arith_brackets_matcher.start();
+                int end = arith_brackets_matcher.end();
+
+                String bracket_part = equation.substring(start + 1, end - 1);
+
+                equation = arith_brackets_matcher.replaceFirst(evaluateSimpleEquation(bracket_part));
+                arith_brackets_matcher.reset(equation);
+                anyPatternFound = true;
+            }
+            if (anyPatternFound) continue;
+
+            arith_no_brackets_matcher.reset(equation);
+            while (arith_no_brackets_matcher.find()){
+                int start = arith_no_brackets_matcher.start();
+                int end = arith_no_brackets_matcher.end();
+
+                String equation_part = equation.substring(start, end);
+
+                equation = arith_no_brackets_matcher.replaceFirst(evaluateSimpleEquation(equation_part));
+                arith_no_brackets_matcher.reset(equation);
+                anyPatternFound = true;
+            }
+            if (anyPatternFound) continue;
 
             // parse lists
+            list_matcher.reset(equation);
             while (list_matcher.find()) {
                 int start = list_matcher.start();
                 int end = list_matcher.end();
@@ -184,5 +231,99 @@ public class Parser {
         }
 
         return equation;
+    }
+
+    private String evaluateSimpleEquation(final @NotNull String str){
+        String s = new String(str);
+        Matcher prior1_matcher = ARITH_PRIOR_1.matcher(s);
+
+        while (prior1_matcher.find()){
+            int start = prior1_matcher.start();
+            int end = prior1_matcher.end();
+
+            String operation = s.substring(start, end);
+            String[] values = operation.split(ARITH_SIGN_PREFIX);
+
+            double op1 = Double.parseDouble(values[0]);
+            double op2 = Double.parseDouble(values[1].substring(1));
+
+            Double res = Math.pow(op1, op2);
+
+            String decimalFormatPattern = String.valueOf(res).replaceAll("[\\d\\-]", "#");
+            DecimalFormat decimalFormat = new DecimalFormat(decimalFormatPattern);
+
+            s = prior1_matcher.replaceFirst(decimalFormat.format(res));
+            prior1_matcher.reset(s);
+        }
+
+        Matcher prior2_matcher = ARITH_PRIOR_2.matcher(s);
+
+        while (prior2_matcher.find()){
+            int start = prior2_matcher.start();
+            int end = prior2_matcher.end();
+
+            String operation = s.substring(start, end);
+            String[] values = operation.split(ARITH_SIGN_PREFIX);
+
+
+            char operator = values[1].charAt(0);
+            values[1] = values[1].substring(1);
+            double op1 = Double.parseDouble(values[0]);
+            double op2 = Double.parseDouble(values[1]);
+
+            Double res = 0.0;
+
+            switch (operator){
+                case '*':{
+                    res = op1 * op2;
+                    break;
+                }
+                case '/':{
+                    res = op1 / op2;
+                    break;
+                }
+            }
+            String decimalFormatPattern = String.valueOf(res).replaceAll("[\\d\\-]", "#");
+            DecimalFormat decimalFormat = new DecimalFormat(decimalFormatPattern);
+
+            s = prior2_matcher.replaceFirst(decimalFormat.format(res));
+            prior2_matcher.reset(s);
+        }
+
+        Matcher prior3_matcher = ARITH_PRIOR_3.matcher(s);
+
+        while (prior3_matcher.find()){
+            int start = prior3_matcher.start();
+            int end = prior3_matcher.end();
+
+            String operation = s.substring(start, end);
+            String[] values = s.split(ARITH_SIGN_PREFIX);
+
+
+            char operator = values[1].charAt(0);
+            values[1] = values[1].substring(1);
+            double op1 = Double.parseDouble(values[0]);
+            double op2 = Double.parseDouble(values[1]);
+
+            Double res = 0.0;
+
+            switch (operator){
+                case '+':{
+                    res = op1 + op2;
+                    break;
+                }
+                case '-':{
+                    res = op1 - op2;
+                    break;
+                }
+            }
+            String decimalFormatPattern = String.valueOf(res).replaceAll("[\\d\\-]", "#");
+            DecimalFormat decimalFormat = new DecimalFormat(decimalFormatPattern);
+
+            s = prior3_matcher.replaceFirst(decimalFormat.format(res));
+            prior3_matcher.reset(s);
+        }
+
+        return s;
     }
 }
